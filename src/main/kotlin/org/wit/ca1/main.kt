6f5ae41db4a85cadcc5a5059806854b6b05565ff
EmmaG1 <emmagrace1352@gmail.com>
2021-10-14 12:51:48 +0100

    Added basic menu

--- src/main/kotlin/org/wit/ca1/main.kt
+++ src/main/kotlin/org/wit/ca1/main.kt
@@ -3,7 +3,40 @@ import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
-fun main(args: Array<String>){
-    println("Recipe Kotlin App Version 1.0")
+fun main(args: Array<String>) {
     logger.info { "Launching Placemark Console App" }
+    println("Placemark Kotlin App Version 1.0")
+
+    var input: Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("You Chose Add Recipe")
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting Down Placemark Console App" }
+}
+
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("Main Menu")
+    println(" 1. Add Recipe")
+    println(" 2. Update Recipe")
+    println(" 3. List All Recipe")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+    return option
 }
