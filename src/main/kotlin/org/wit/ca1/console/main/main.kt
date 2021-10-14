cd9ce22d8f772dfb62a86f6666cad7cf4b1773f4
EmmaG1 <emmagrace1352@gmail.com>
2021-10-14 14:22:36 +0100

    end of lab2

--- src/main/kotlin/org/wit/ca1/console/main/main.kt
+++ src/main/kotlin/org/wit/ca1/console/main/main.kt
@@ -6,7 +6,7 @@ import org.wit.ca1.console.models.RecipeModel
 
 private val logger = KotlinLogging.logger {}
 
-var recipe = RecipeModel()
+//var recipe = RecipeModel()
 val recipes = ArrayList<RecipeModel>()
 
 fun main(args: Array<String>) {
@@ -21,6 +21,8 @@ fun main(args: Array<String>) {
             1 -> addRecipe()
             2 -> updateRecipe()
             3 -> listRecipes()
+            4 -> searchRecipe()
+            -99 -> dummyData()
             -1 -> println("Exiting App")
             else -> println("Invalid Option")
         }
@@ -38,6 +40,7 @@ fun menu() : Int {
     println(" 1. Add Recipe")
     println(" 2. Update Recipe")
     println(" 3. List All Recipes")
+    println(" 4. Search Recipes")
     println("-1. Exit")
     println()
     print("Enter Option : ")
@@ -50,37 +53,61 @@ fun menu() : Int {
 }
 
 fun addRecipe(){
+    var aRecipe = RecipeModel()
     println("Add Recipe")
     println()
     print("Enter a Title : ")
-    recipe.title = readLine()!!
+    aRecipe.title = readLine()!!
     print("Enter Ingredients : ")
-    recipe.ingred = readLine()!!
+    aRecipe.ingred = readLine()!!
 
-    if (recipe.title.isNotEmpty() && recipe.ingred.isNotEmpty()) {
-        recipes.add(recipe.copy())
-        logger.info("Recipe Added : [ $recipe ]")
+    if (aRecipe.title.isNotEmpty() && aRecipe.ingred.isNotEmpty()) {
+        aRecipe.id = recipes.size.toLong()
+        recipes.add(aRecipe.copy())
+        logger.info("Recipe Added : [ $aRecipe ]")
     }
     else
         logger.info("Recipe Not Added")
-    recipe.id++
 }
 
 fun updateRecipe() {
     println("Update Recipe")
     println()
-    print("Enter a new Title for [ " + recipe.title + " ] : ")
-    recipe.title = readLine()!!
-    print("Enter updated ingredients for [ " + recipe.ingred + " ] : ")
-    recipe.ingred = readLine()!!
-    println("You updated [ " + recipe.title + " ] for title " +
-            "and [ " + recipe.ingred + " ] for ingredients")
+    listRecipes()
+    var searchId = getId()
+    val aRecipe = search(searchId)
+
+    if(aRecipe != null) {
+        print("Enter a new Title for [ " + aRecipe.title + " ] : ")
+        aRecipe.title = readLine()!!
+        print("Enter a new Description for [ " + aRecipe.ingred + " ] : ")
+        aRecipe.ingred = readLine()!!
+        println(
+            "You updated [ " + aRecipe.title + " ] for title " +
+                    "and [ " + aRecipe.ingred + " ] for description"
+        )
+    }
+    else
+        println("Recipe Not Updated...")
+
 }
 
 fun listRecipes() {
     println("List All Recipes")
     println()
     recipes.forEach { logger.info("${it}") }
+    println()
+}
+
+fun searchRecipe() {
+
+    var searchId = getId()
+    val aRecipe = search(searchId)
+
+    if(aRecipe != null)
+        println("Recipe Details [ $aRecipe ]")
+    else
+        println("Recipe Not Found...")
 }
 
 fun getId() : Long {
@@ -96,6 +123,12 @@ fun getId() : Long {
 }
 
 fun search(id: Long) : RecipeModel? {
-    var foundPlacemark: RecipeModel? = recipes.find { p -> p.id == id }
-    return foundPlacemark
+    var foundRecipe: RecipeModel? = recipes.find { p -> p.id == id }
+    return foundRecipe
+}
+
+fun dummyData() {
+    recipes.add(RecipeModel(1, "New York New York", "So Good They Named It Twice"))
+    recipes.add(RecipeModel(2, "Ring of Kerry", "Some place in the Kingdom"))
+    recipes.add(RecipeModel(3, "Waterford City", "You get great Blaas Here!!"))
 }
